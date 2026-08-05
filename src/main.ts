5571bab0af925f2b86f496fd2a20da6ed2cd8f37
David Behling <david_behling@hotmail.com>
2026-08-05 10:45:28 -0300

    feat: configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Employee Registration API')
+    .setDescription('API para gerenciamento de colaboradores e documentos.')
+    .setVersion('1.0')
+    .addTag('employee')
+    .addTag('document-type')
+    .addTag('employee-required-document')
+    .addTag('employee-document-version')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
