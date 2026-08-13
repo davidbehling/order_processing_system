3312377162251abcd4004a4242e1312aafa26b26
David Behling <david_behling@hotmail.com>
2026-08-13 09:28:52 -0300

    feat(auth): adiciona guard global e rotas públicas

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,21 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
-import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { CurrentUser } from './decorators/current-user.decorator';
 import type { AuthUser } from './interfaces/auth-user.interface';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('login')
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('profile')
   profile(@CurrentUser() user: AuthUser) {
     return user;
